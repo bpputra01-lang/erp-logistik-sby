514db56c97426ea2386f2ab482804f2aedb673da
bpputra01-lang <bpputra01@gmail.com>
2026-02-18 20:57:37 +0700

    asdas

--- app.py
+++ app.py
@@ -184,8 +184,8 @@ with st.sidebar:
 if menu == "📥 Putaway System":
     st.markdown('<div class="hero-header"><h1>📥 PUTAWAY SYSTEM PRO (MACRO LOGIC)</h1></div>', unsafe_allow_html=True)
     c1, c2 = st.columns(2)
-    with c1: up_ds = st.file_uploader("Upload DS PUTAWAY", type=['xlsx'])
-    with c2: up_asal = st.file_uploader("Upload ASAL BIN PUTAWAY", type=['xlsx'])
+    with c1: up_ds = st.file_uploader("Upload DS PUTAWAY", type=['xlsx', 'csv'])
+    with c2: up_asal = st.file_uploader("Upload ASAL BIN PUTAWAY", type=['xlsx', 'csv'a])
     
     if up_ds and up_asal:
         if st.button("⚡ JALANKAN PROSES PUTAWAY"):
