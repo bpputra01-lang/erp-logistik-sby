3119087317730afe2377b3ea44e30af665e25428
bpputra01-lang <bpputra01@gmail.com>
2026-02-19 13:25:56 +0700

    SDS

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ with st.sidebar:
         section[data-testid="stSidebar"] .stCaption p {
             color: #E2E8F0; /* Slate Gray Light yang mewah */
             font-family: 'Inter', sans-serif;
-             font-size: 10px;
+             font-size: 13px;
             opacity: 1 !important;
         }
     </style>
