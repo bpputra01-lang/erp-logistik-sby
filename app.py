1900df0f0ee723ffb1b9e2e6e4a6b428d8d4f6d8
bpputra01-lang <bpputra01@gmail.com>
2026-02-21 01:09:04 +0700

    WQE

--- app.py
+++ app.py
@@ -236,8 +236,8 @@ with st.sidebar:
 
         /* FIX 2: LABEL DI ATAS BOX JADI HITAM (BIAR KELIHATAN) */
         div[data-testid="stWidgetLabel"] p {
-            color: #FFFFFF !important;
-            -webkit-text-fill-color: #000000 !important;
+            color: ##FFFFFF !important;
+            -webkit-text-fill-color: #FFFFFF !important;
             font-family: 'Inter', sans-serif;
             font-weight: bold;
         }
