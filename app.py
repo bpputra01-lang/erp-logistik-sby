5edba5b4c1683a21e67fb000c8d0faf146e127a1
bpputra01-lang <bpputra01@gmail.com>
2026-02-21 19:27:49 +0700

    ddd

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ if not st.session_state.logged_in:
             <h2 style="
                 color: #C5A059; 
                 margin-top: -30px; 
-                margin-bottom: -15px; 
+                margin-bottom: -30px; 
                 font-family: 'Inter', sans-serif; 
                 font-weight: 800; 
                 letter-spacing: 1px;
