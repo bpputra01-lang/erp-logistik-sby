1d214d4c1f4529bd18a7808ec2ac8ce1a85425b3
bpputra01-lang <bpputra01@gmail.com>
2026-02-21 13:18:13 +0700

    ss

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if not st.session_state.logged_in:
             background: linear-gradient(rgba(30, 30, 47, 0.9), rgba(30, 30, 47, 0.9)), 
                         url('https://images.unsplash.com/photo-1586528116311-ad8dd3c8310d?q=80&w=2070'); /* Background Logistik */
             background-size: cover;
-            background-position: center;
+            background-position: fixed;
         }
 
         /* Container Login Box agar pas di tengah */
