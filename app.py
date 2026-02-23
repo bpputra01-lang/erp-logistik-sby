158016a19b9b7efe8c49f7316423d813eeea5ce3
bpputra01-lang <bpputra01@gmail.com>
2026-02-24 01:46:33 +0700

    asd

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ div.stFormSubmitButton > button {
         st.markdown("""
   <div style="
     display: block; 
-    width: 160%; 
+    width: 150%; 
     text-align: center; 
     margin: 0 auto;
     overflow: visible;
