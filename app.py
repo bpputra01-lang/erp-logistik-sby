47525dafa252812b838a2a1dbcff44d53a913886
bpputra01-lang <bpputra01@gmail.com>
2026-02-24 02:37:59 +0700

    asd

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ st.markdown("""
     [data-testid="stSidebarUserContent"] { padding-top: 0rem !important; }
     [data-testid="stSidebarNav"] { display: none; } 
     
-    /* SATU BARIS SAKTI: Paksa semua kontainer input (Username & Password) jadi lebar penuh */
-div[data-baseweb="input"], [data-testid="stForm"] [data-testid="column"] {
-    width: 100% !important; min-width: 100% !important; flex-basis: 100% !important;
-}
     
     /* 2. STYLE JUDUL ERP DI SIDEBAR */
     .sidebar-title { 
@@ -175,6 +171,10 @@ div[data-baseweb="input"], [data-testid="stForm"] [data-testid="column"] {
         color: #1a2634 !important;
         font-weight: bold !important;
     }
+    /* SATU BARIS SAKTI: Paksa semua kontainer input (Username & Password) jadi lebar penuh */
+div[data-baseweb="input"], [data-testid="stForm"] [data-testid="column"] {
+    width: 100% !important; min-width: 100% !important; flex-basis: 100% !important;
+}
     </style>
     """, unsafe_allow_html=True)
     # --- JANGAN UBAH KODE DI ATAS, TAMBAHKAN DI BAWAHNYA ---
