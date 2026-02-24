63b97ac7e2dee3bc7cc7950403d275967a297e6f
bpputra01-lang <bpputra01@gmail.com>
2026-02-24 18:58:54 +0700

    asd

--- app.py
+++ app.py
@@ -492,7 +492,7 @@ from openpyxl import load_workbook
 # 1. FUNGSI PENDUKUNG (HARUS DI ATAS)
 # =========================================================
 
-def compare_data_scan_stock_system(df_scan, df_stock, scan_file):
+def Stock_Opname(df_scan, df_stock, scan_file):
     try:
         # VBA: Scan BIN(A), SKU(B), QTY(C)
         df_scan_clean = df_scan.iloc[:, [0, 1, 2]].copy()
