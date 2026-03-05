e2f5837fdfae287cd7bc86def2c8323d0a2ff5c8
bpputra01-lang <bpputra01@gmail.com>
2026-03-05 23:13:54 +0700

    cek karantina

--- app.py
+++ app.py
@@ -609,7 +609,7 @@ def logic_setup_karantina_with_check(df_outstanding):
     df.iloc[:, 14] = pd.to_numeric(df.iloc[:, 14], errors='coerce').fillna(0)
     df['CHECK_DIFF'] = df.iloc[:, 10] - df.iloc[:, 14]
     
-    df_check = df.iloc[:, [2, 3, 10, 14]].copy() 
+    df_check = df.iloc[:, [1, 2, 9, 13]].copy() 
     df_check.columns = ['BIN', 'SKU', 'QTY_SYSTEM_K', 'HASIL_REKON_O']
     df_check['SELISIH_HITUNG_AI'] = df['CHECK_DIFF']
     
