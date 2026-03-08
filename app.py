3e94e283eea13bb1560ccfb407e991d7ce7282a7
bpputra01-lang <bpputra01@gmail.com>
2026-03-09 00:49:26 +0700

    SDS

--- app.py
+++ app.py
@@ -432,8 +432,8 @@ div.stFormSubmitButton > button {
         st.markdown("""
             <h2 style="
                 color: #C5A059; 
-                margin-top: -60px; 
-                margin-bottom: -100px; 
+                margin-top: -300px; 
+                margin-bottom: -5px; 
                 font-family: 'Inter', sans-serif; 
                 font-weight: 800; 
                 text-align: center;
