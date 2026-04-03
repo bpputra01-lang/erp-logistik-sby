f39bd4c8759f1bb69e689e1b18a8d0453fbbbf15
bpputra01-lang <bpputra01@gmail.com>
2026-04-03 20:37:00 +0700

    ADAD

--- app.py
+++ app.py
@@ -3161,6 +3161,7 @@ def project_approval_reject():
         label { color: #E0E0E0 !important; font-weight: 600 !important; }
         </style>
     """, unsafe_allow_html=True)
+    st.markdown('<div class="hero-header">📋PENGAJUAN REJECT/DEFECT</div>', unsafe_allow_html=True)
     # --- FORM INPUT ---
     with st.form("form_reject"):
         col1, col2 = st.columns(2)
