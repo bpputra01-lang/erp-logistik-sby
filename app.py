014c03971cce983720d6d67b04a0fd16b8596bc1
bpputra01-lang <bpputra01@gmail.com>
2026-04-05 14:19:09 +0700

    ADAD

--- app.py
+++ app.py
@@ -2688,6 +2688,15 @@ def menu_retur_out_system():
                 conn.commit()
                 st.success("Data berhasil dihapus!")
                 st.rerun()
+        else:
+            # Muncul hanya jika query return nol baris
+            st.info("Database masih kosong. Silakan upload file di atas.")
+
+    except Exception as e:
+        st.error(f"Sistem Gagal Memuat Database: {e}")
+    finally:
+        # Menutup koneksi dengan aman
+        conn.close()
 
 
 def process_justification(df_case, df_tracking, df_po):
