fddcf68a718bc0be539d2c6828d0714f00166b1c
bpputra01-lang <bpputra01@gmail.com>
2026-05-03 13:02:43 +0700

    qqq

--- app.py
+++ app.py
@@ -3761,7 +3761,7 @@ st.markdown("""
 
 
 # --- 4. MAIN UI ---
-    st.markdown('<div class="hero-header">🚹 REPORTING & PIC - JEZPRO</div>', unsafe_allow_html=True)
+st.markdown('<div class="hero-header">🚹 REPORTING & PIC - JEZPRO</div>', unsafe_allow_html=True)
 
     list_pic =["VERREL & GALIH", "FARIL & YUDI", "BAKCLINER", "VANO", "HAMZAH", "KRISNA & DHIVA", "WAREHOUSE FULLFILLMENT"]
     current_user = st.selectbox("👤 Pilih Nama:", list_pic)
