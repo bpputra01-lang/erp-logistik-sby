9613f32b7895463231b96dddf06e0e426508cb95
bpputra01-lang <bpputra01@gmail.com>
2026-08-23 13:22:51 +0700

    sss

--- erp-reflex-sby/rxconfig.py
+++ erp-reflex-sby/rxconfig.py
@@ -2,7 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="erp_reflex_sby",
-    favicon="/image_981625.png",
     plugins=[
         rx.plugins.RadixThemesPlugin(theme=rx.theme(appearance="dark", accent_color="red")),
     ],
