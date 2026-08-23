b151a27fc526fb8a998e20fada14060c069c795b
bpputra01-lang <bpputra01@gmail.com>
2026-08-23 13:12:52 +0700

    aaa

--- erp-reflex-sby/rxconfig.py
+++ erp-reflex-sby/rxconfig.py
@@ -2,7 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="erp_reflex_sby",
-    plugins=[
-        rx.plugins.RadixThemesPlugin(theme=rx.theme(appearance="dark", accent_color="red")),
-    ],
+    favicon="/image_981625.png", # Mengarah langsung ke file di folder assets
 )
