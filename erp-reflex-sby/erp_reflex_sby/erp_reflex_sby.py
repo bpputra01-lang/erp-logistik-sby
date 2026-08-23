b48ebe9d2e6c3ed5b553cbcf5f0255ee7c256ab9
bpputra01-lang <bpputra01@gmail.com>
2026-08-23 11:09:03 +0700

    ccc

--- erp-reflex-sby/erp_reflex_sby/erp_reflex_sby.py
+++ erp-reflex-sby/erp_reflex_sby/erp_reflex_sby.py
@@ -246,16 +246,23 @@ def login_page() -> rx.Component:
                 rx.text("Silakan masuk dengan akun resmi gudang Anda.", size="2", color="#B0B0B0"),
 
                 # Input Username (Diperlebar)
+                # Input Password
                 rx.vstack(
-                    rx.text("USERNAME", size="1", font_weight="700", color="#FFFFFF", letter_spacing="1px"),
+                    rx.text("PASSWORD", size="1", font_weight="700", color="#FFFFFF", letter_spacing="1px"),
                     rx.input(
-                        placeholder="Masukkan username...",
-                        value=AppState.username,
-                        on_change=AppState.set_username,
+                        type="password",
+                        placeholder="Masukkan password...",
+                        value=AppState.password,
+                        on_change=AppState.set_password,
+                        # KODE BARU: BISA ENTER UNTUK LOGIN
+                        on_key_down=rx.cond(
+                            rx.event.key == "Enter",
+                            AppState.handle_login
+                        ),
                         size="3",
                         variant="surface",
                         color_scheme="red",
-                        width="100%",  # Full width proporsional
+                        width="100%",
                         style={
                             "background": "rgba(0, 0, 0, 0.75)", 
                             "border": "1px solid rgba(229, 9, 20, 0.4)", 
