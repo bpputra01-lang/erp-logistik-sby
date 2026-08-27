8d52af3e2686f84acdd48e6e85185d3105144d26
bpputra01-lang <bpputra01@gmail.com>
2026-08-28 00:27:00 +0700

    clean rxconfig and deploy to fresh app instance

--- erp-reflex-sby/rxconfig.py
+++ erp-reflex-sby/rxconfig.py
@@ -1,12 +1,5 @@
-import reflex as rx
+﻿import reflex as rx
 
 config = rx.Config(
     app_name="erp_reflex_sby",
-    favicon="/favicon.png",
-    show_reflex_badge=False,
-    plugins=[
-        rx.plugins.RadixThemesPlugin(
-            theme=rx.theme(appearance="dark", accent_color="red")
-        ),
-    ],
 )
