1129159061b794b9d4527281973ab7e2e7e48543
bpputra01-lang <bpputra01@gmail.com>
2026-08-29 21:19:01 +0700

    sss

--- shiny_code/shiny_app.py
+++ shiny_code/shiny_app.py
@@ -4,7 +4,7 @@ from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 from state import AppState
 from views import (
     CUSTOM_HEAD, static_loading_spinner, success_modal, error_modal,
-    render_clean_table, metric_box, dark_metric_box,   # <-- Tambahkan metric_box & dark_metric_box di sini
+    render_clean_table, metric_box, dark_metric_box,  BRANCH_BIN_MAPPING, 
     compare_system_view, stock_minus_view,
     putaway_view, main_dashboard_view, sidebar, cycle_count_view, login_page, ppa_audit_view, cycle_count_analyzer_view, global_header
 )
